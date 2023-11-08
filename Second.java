305042d27f5e67faaa03b12350a5411633bdcb59
Phuc Nguyen Lamp <giaphuc29082004@gmail.com>
2023-11-08 14:29:52 +0700

    mot dong cut

---
+++ Second.java
@@ -0,0 +1,9 @@
+import mypack.*;
+
+// Code from filename: Second.java
+class Second {
+  public static void main(String[] args) {
+    package1 obj = new package1();
+    obj.main(args);
+  }
+}
---
+++ mypack/package1.java
@@ -0,0 +1,6 @@
+package mypack;
+public class package1 {
+    public void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
